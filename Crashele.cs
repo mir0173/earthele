924a5228f9bd4fe8e8ad2dbb02def87c83269a3f
mir0173 <83905125+mir0173@users.noreply.github.com>
2023-10-22 15:46:15 +0900

    Update Crashele.cs

--- Crashele.cs
+++ Crashele.cs
@@ -26,7 +26,7 @@ public class Crashele : MonoBehaviour
 
         if (collidedObject.CompareTag("Sun"))
         {
-            scale = Mathf.Sqrt(Mathf.Pow(collidedObject.transform.localScale.x, 2f) + Mathf.Sqrt(myscale));
+            scale = Mathf.Pow(Mathf.Pow(collidedObject.transform.localScale.x, 3f) + Mathf.Pow(myscale, 3f), 0.33333f);
             collidedObject.transform.localScale = new Vector3(scale, scale, scale);
             collidedObject.GetComponent<Rigidbody>().mass = Mathf.Pow(scale, 2.5f);
         }
@@ -37,7 +37,7 @@ public class Crashele : MonoBehaviour
             {
                 ContactPoint contact = collision.contacts[0];
                 Vector3 pos = contact.point;
-                scale = Mathf.Sqrt(Mathf.Pow(collidedObject.transform.localScale.x, 2) + Mathf.Sqrt(myscale));
+                scale = Mathf.Pow(Mathf.Pow(collidedObject.transform.localScale.x, 3) + Mathf.Pow(myscale, 3), 0.33333f);
                 collidedObject.transform.localScale = new Vector3(scale, scale, scale);
                 collidedObject.GetComponent<Rigidbody>().mass = Mathf.Pow(scale, 2.5f);
                 collidedObject.GetComponent<Rigidbody>().AddForce((pos * collidedObject.GetComponent<Rigidbody>().mass / (collidedObject.GetComponent<Rigidbody>().mass + gameObject.GetComponent<Rigidbody>().mass)), ForceMode.Force);
@@ -46,3 +46,4 @@ public class Crashele : MonoBehaviour
         } 
     }
 }
+
